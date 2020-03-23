a67914580bb59834a549bd78a0eb48c85076472a
varsarius <43183178+varsarius@users.noreply.github.com>
2020-03-23 15:45:02 +0200

    Add files via upload

---
+++ index.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import time
+from discord.utils import get
+from discord import message
+from discord.guild import Guild
+import os
+client = discord.Client()
+bot = commands.Bot(command_prefix='/')
+
+@bot.command(pass_context=True) #разрешаем передавать агрументы
+async def echo(ctx, arg): #создаем асинхронную фунцию бота
+    await ctx.send(arg) #отправляем обратно аргумент
+
+@bot.event
+async def on_member_join(member):
+    channel = client.get_channel(690973529985777754)
+    role = discord.utils.get(member.guild.roles, id=690966191581429912)
+    await member.add_roles(role)
+
+@bot.command()
+@commands.has_any_role("Учитель", "Admin")
+async def operm(ctx):
+    for role in ctx.guild.roles:
+        if role.name == "Ученик":
+            await ctx.channel.set_permissions(role, send_messages=True)
+            await ctx.channel.send("Ученикам теперь разрешено отправлять сообщения на этом канале");
+
+@bot.command()
+@commands.has_any_role("Учитель", "Admin")
+async def cperm(ctx):
+    for role in ctx.guild.roles:
+        if role.name == "Ученик":
+            await ctx.channel.set_permissions(role, send_messages=False)
+            await ctx.channel.send("Ученикам теперь запрещено отправлять сообщения на этом канале");
+
+@bot.command(pass_context=False)
+@commands.has_role("Учитель")
+async def openbio(ctx):
+    print(Role("Учитель"))
+
+
+token = os.environ.get('BOT_TOKEN')
